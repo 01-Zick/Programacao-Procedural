af5927e4f66de9ec67a1e6f18e9b1c41f2628f82
01-Zick <zickmanhozo@gamil.com>
2024-03-01 18:58:04 -0300

    Lista2

---
+++ Lista2/Exercicio1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("digite o nome do usuario: ");
+    string usuario = Console.ReadLine();
+
+    Console.WriteLine("digite a senha: ");
+    string senha = Console.ReadLine();
+
+    if (usuario == "admin" && senha == "123senha")
+    {
+      Console.WriteLine("Login realizado com sucesso!");
+    }
+    else
+    {
+      Console.WriteLine("Credenciais invalidas");
+    }
+  }
+}
