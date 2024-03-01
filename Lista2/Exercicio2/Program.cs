9e33de260e4005db67ec6b1c6fd5c705d5dc6900
Zick <159500555+01-Zick@users.noreply.github.com>
2024-03-01 19:14:18 -0300

    Update Program.cs

--- Lista2/Exercicio2/Program.cs
+++ Lista2/Exercicio2/Program.cs
@@ -1,1 +1,22 @@
-﻿
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Digite o nome: ");
+      string nome = (Console.ReadLine());
+    
+      if (nome == "Joselito")
+      {
+        Console.WriteLine("Rotina de Joselito: \n 1. Escola das 07h as 12h \n 2. Almoço das 12h as 13h                        3. Futebol das 14h as 16h \n 4. Dever de casa das 16h as 18h");
+      }
+        else if (nome == "Filomena")
+        {
+           Console.WriteLine("Rotina de Filomena: \n 1. Escola das 07h as 12h \n 2. Almoço das 12h as 13h                        3. Futebol das 14h as 16h \n 4. Dever de casa das 16h as 18h");
+        }
+  }
+    
+}﻿
