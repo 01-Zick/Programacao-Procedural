a835b10a0e62a3c299a327948f87a84643b17422
01-Zick <zickmanhozo@gmail.com>
2024-04-11 07:01:16 -0300

    Lista8

---
+++ Lista8/ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+struct Pizza
+{
+    public int comanda;
+    public string sabor;
+    public double preco;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pizza pedido1;
+        pedido1.comanda = Convert.ToInt32(Console.ReadLine());
+        pedido1.sabor = Console.ReadLine();
+        pedido1.preco = Convert.ToDouble(Console.ReadLine());
+
+        System.Console.WriteLine(pedido1.comanda);
+        System.Console.WriteLine(pedido1.sabor);
+        System.Console.WriteLine(pedido1.preco);
+    }
+}
---
+++ Lista8/ex2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Dynamic;
+struct Produto
+{
+    public int codigo;
+    public string nome;
+    public double preco;
+    public bool disponibilidade;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int TAMANHO_VETOR = 4;
+        Produto[] produtos = new Produto[TAMANHO_VETOR];
+
+        for (int i = 0; i < produtos.Length; i++)
+        {
+            produtos[i].codigo = Convert.ToInt32(Console.ReadLine());
+            produtos[i].nome = Console.ReadLine();
+            produtos[i].preco = Convert.ToDouble(Console.ReadLine());
+            produtos[i].disponibilidade = Convert.ToBoolean(Console.ReadLine());
+        }
+        for (int i = 0; i < produtos.Length; i++)
+        {
+            if (produtos[i].disponibilidade == true)
+            {
+                Console.WriteLine(produtos[i].nome);
+                Console.WriteLine(produtos[i].preco);
+            }
+        }
+    }
+}
